40b5d50ec1789334a01161134d28b279817ddfcb
oliverjm1 <75314670+oliverjm1@users.noreply.github.com>
2023-11-07 17:58:06 +0000

    further train stuff

--- src/cartPole/cartPole_train.py
+++ src/cartPole/cartPole_train.py
@@ -27,3 +27,47 @@ plt.ion()
 
 # if GPU is to be used
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Define a transition from one state to the next
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
+
+"""
+This is a class to store transitions that are observed.
+This is then randomly sampled from for training.
+Sampling randomly increases stability.
+"""
+
+class ReplayMemory(object):
+
+    def __init__(self, capacity):
+        self.memory = deque([], maxlen=capacity)
+
+    def push(self, *args):
+        """Save a transition"""
+        self.memory.append(Transition(*args))
+
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+
+    def __len__(self):
+        return len(self.memory)
+    
+"""
+Deep Q Network (DQN) which will try to approximate the Q function.
+"""
+
+class DQN(nn.Module):
+
+    def __init__(self, n_observations, n_actions):
+        super(DQN, self).__init__()
+        self.layer1 = nn.Linear(n_observations, 128)
+        self.layer2 = nn.Linear(128, 128)
+        self.layer3 = nn.Linear(128, n_actions)
+
+    # Called with either one element to determine next action, or a batch
+    # during optimization. Returns tensor([[left0exp,right0exp]...]).
+    def forward(self, x):
+        x = F.relu(self.layer1(x))
+        x = F.relu(self.layer2(x))
+        return self.layer3(x)
