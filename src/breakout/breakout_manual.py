3e906ce441c29ba0f71655810ab2dd7bb6cb2455
oliverjm1 <75314670+oliverjm1@users.noreply.github.com>
2023-11-07 17:27:01 +0000

    resizing of breakout screen, renaming pre-having a go at training cartPole

--- src/breakout/breakout_manual.py
+++ src/breakout/breakout_manual.py
@@ -2,12 +2,13 @@ import numpy as np
 import gym
 from gym.utils.play import play, PlayPlot
 
-print(gym.envs.registration.registry.keys())
 env = gym.make("ALE/Breakout-v5", full_action_space=False)
 n_actions = env.action_space
 print("num actions: ", n_actions)
 # Get the number of state observations
 state, info = env.reset()
+print("state: ",state)
+print("state shape: ",state.shape)
 n_observations = len(state)
 print("num obs: ", n_observations)
 
@@ -15,4 +16,4 @@ play(gym.make("ALE/Breakout-v5", render_mode="rgb_array"), keys_to_action={
     "s": np.array(1),
     "d": np.array(2),
     "a": np.array(3),
-}, noop=np.array(0))
+}, noop=np.array(0), zoom=2.5)
